cf3d471c8a51672c43f5f4855daec80c5e08583c
Fábio Santos <ffsantos92@gmail.com>
2015-01-25 22:27:00 +0000

    Initial commit

---
+++ create_countries_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCountriesTable extends Migration {
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('countries', function(Blueprint $table)
+        {
+            $table->increments('id');
+            $table->string('code');
+            $table->string('name');
+        });
+
+    }
+
+    /**
+    * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('countries');
+    }
+
+}
