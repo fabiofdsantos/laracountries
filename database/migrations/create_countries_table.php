aa2e89c2dc51f551fa00a0f19bc3751badd4d54a
ffsantos92 <ffsantos92@gmail.com>
2015-07-10 16:59:55 +0100

    Fix CS

--- database/migrations/create_countries_table.php
+++ database/migrations/create_countries_table.php
@@ -1,10 +1,10 @@
 <?php
 
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
 
-class CreateCountriesTable extends Migration {
-
+class CreateCountriesTable extends Migration
+{
     /**
      * Run the migrations.
      *
@@ -12,13 +12,11 @@ class CreateCountriesTable extends Migration {
      */
     public function up()
     {
-        Schema::create('countries', function(Blueprint $table)
-        {
+        Schema::create('countries', function (Blueprint $table) {
             $table->increments('id');
             $table->string('code');
             $table->string('name');
         });
-
     }
 
     /**
@@ -30,5 +28,4 @@ class CreateCountriesTable extends Migration {
     {
         Schema::drop('countries');
     }
-
 }
